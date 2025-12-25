4372ffc9cac1240f12b16f2cb089147cff7352eb
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-12-25 22:20:30 +0800

    explicit enabling of force mono chrome app icons
    Change-Id: I76d394bee00571829e6df0fa0f95f1f28bda9729
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- iconloaderlib/src/com/android/launcher3/icons/mono/MonoIconThemeController.kt
+++ iconloaderlib/src/com/android/launcher3/icons/mono/MonoIconThemeController.kt
@@ -92,7 +92,7 @@ class MonoIconThemeController(
         if (mono != null) {
             return ClippedMonoDrawable(mono, shapePath)
         }
-        if (Flags.forceMonochromeAppIcons() && shouldForceThemeIcon && !isFileDrawable) {
+        if (!isFileDrawable) {
             return MonochromeIconFactory(info.icon.width).wrap(base, shapePath)
         }
         return null
